9b34cef57d143f99b6a96b205dfb3904be1dde48
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-12-06 16:09:47 +0100

    getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps, NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { CntrlClient, Page, PageProps, cntrlSdkContext } from '@gxpl/sdk';
 
 const client = new CntrlClient(process.env.CNTRL_API_URL!);
@@ -13,7 +13,7 @@ const CntrlPage: NextPage<PageProps> = (props) => {
   );
 }
 
-export const getStaticProps: GetStaticProps<PageProps> = async () => {
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
   const props = await client.getPageData();
   return { props };
 };
