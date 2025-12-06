a7e633814fda9dcfcd142b7dd5317e32a6565e6a
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-12-06 16:14:06 +0100

    change config for staging

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const isCodeExport = process.env.CNTRL_BUILD_MODE === 'self-hosted';
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  output: 'export',
-  distDir: '_static',
   assetPrefix: isCodeExport ? './' : undefined,
 };
 
