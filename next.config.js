9e60b186630da82963b283786cb21233dfa87199
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2025-12-11 19:35:59 +0700

    base path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const isCodeExport = process.env.CNTRL_BUILD_MODE === 'self-hosted';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
   assetPrefix: isCodeExport ? './' : undefined,
+  basePath,
 };
 
 module.exports = nextConfig;
