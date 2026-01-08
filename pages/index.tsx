21889154e1573a1ea9b6de354f9890721bbeaf14
sergokiko <sergokiko@gmail.com>
2026-01-08 16:15:41 +0100

    code export

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next';
 import { CntrlClient, Page, PageProps, cntrlSdkContext } from '@gxpl/sdk';
 
 const client = new CntrlClient(process.env.CNTRL_API_URL!);
+const buildMode = process.env.CNTRL_BUILD_MODE!;
 
 const CntrlPage: NextPage<PageProps> = (props) => {
   cntrlSdkContext.init({
@@ -14,7 +15,7 @@ const CntrlPage: NextPage<PageProps> = (props) => {
 }
 
 export const getStaticProps: GetStaticProps<PageProps> = async () => {
-  const props = await client.getPageData();
+  const props = await client.getPageData(buildMode === 'self-hosted' ? 'self-hosted' : 'default');
   return { props };
 };
 
